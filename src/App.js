64f042fd3af9e06f0f3050ec6dab29c57f25ec2c
Kasra Naderi <kasra@yuser.co>
2022-06-03 13:25:37 -0400

    fix error and add Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
