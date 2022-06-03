6652b815d4dfbba80a15a9e976a18ed042836f1d
Kasra Naderi <kasra@yuser.co>
2022-06-03 13:15:06 -0400

    import pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import Home from
+import Home from './pages/Home';
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Projects from './pages/Projects';
 
-  function App() {
-    return (
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
