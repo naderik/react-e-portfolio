ba00ce24847d7abe5cd302d13f30d83bac73c8b3
Kasra Naderi <kasra@yuser.co>
2022-06-03 13:30:11 -0400

    add Links to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className='navbar-button'>
+                <button>
+                </button>
+            </div>
+            <div className='navbar-links'>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/resume'>Resume</Link>
+                <Link to='/projects'>Projects</Link>
+            </div>
+        </div>
     )
 }
 
