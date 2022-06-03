462ca9ff635646d48fad89cb323ab0773c7900e0
Kasra Naderi <kasra@yuser.co>
2022-06-03 13:32:34 -0400

    add Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Resume from './pages/Resume';
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
