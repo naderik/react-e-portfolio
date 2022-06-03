0e220c163c59125c32727607fd1091237d3c7c77
Kasra Naderi <kasra@yuser.co>
2022-06-03 09:22:48 -0400

    small fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -61,13 +61,13 @@ function NavBar() {
                 </Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto" defaultActiveKey="#home">
-                        <Nav.Item>
+                        {/* <Nav.Item>
                             <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
                                 <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
                             </Nav.Link>
-                        </Nav.Item>
+                        </Nav.Item> */}
 
-                        <Nav.Item>
+                        {/* <Nav.Item>
                             <Nav.Link
                                 as={Link}
                                 to="/about"
@@ -75,9 +75,9 @@ function NavBar() {
                             >
                                 <AiOutlineUser style={{ marginBottom: "2px" }} /> About
                             </Nav.Link>
-                        </Nav.Item>
+                        </Nav.Item> */}
 
-                        <Nav.Item>
+                        {/* <Nav.Item>
                             <Nav.Link
                                 as={Link}
                                 to="/project"
@@ -88,9 +88,9 @@ function NavBar() {
                                 />{" "}
                                 Projects
                             </Nav.Link>
-                        </Nav.Item>
+                        </Nav.Item> */}
 
-                        <Nav.Item>
+                        {/* <Nav.Item>
                             <Nav.Link
                                 as={Link}
                                 to="/resume"
@@ -98,7 +98,7 @@ function NavBar() {
                             >
                                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
                             </Nav.Link>
-                        </Nav.Item>
+                        </Nav.Item> */}
 
                         <Nav.Item>
                             <Nav.Link
