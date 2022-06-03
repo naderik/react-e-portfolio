8aa00a64451ecf604d322bfc3018298edbe4468d
Kasra Naderi <kasra@yuser.co>
2022-06-03 14:31:56 -0400

    add button for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../styles/Navbar.css'
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
+    const [expandNav, setExpandNav] = React.useState(false);
+    const location = useLocation();
+    React.useEffect(() => {
+        setExpandNav(false);
+    }, [location]);
     return (
-        <div className='navbar'>
+        <div className='navbar' id={expandNav ? "open" : "close"}>
             <div className='toggleButton'>
-                <button>
+                <button onClick={
+                    () => {
+                        setExpandNav((prev) => !prev)
+                    }}>
+                    <MenuIcon />
                 </button>
             </div>
             <div className='links'>
