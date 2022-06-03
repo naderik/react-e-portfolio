05e7d2258ae6b6337cf1a69bcc900a5a1ae01f0e
Kasra Naderi <kasra@yuser.co>
2022-06-03 14:45:28 -0400

    add social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div>
+                <GitHubIcon />
+                <LinkedInIcon />
+                <TwitterIcon />
+            </div>
+            <p> &copy; 2022 naderik </p>
+        </div>
     )
 }
 
