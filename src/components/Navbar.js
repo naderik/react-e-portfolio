272656cdcce9061f5bc36bdf2fa918c9dbfa4ecb
Kasra Naderi <kasra@yuser.co>
2022-06-03 13:40:06 -0400

    add more css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/Navbar.css'
 
 function Navbar() {
     return (
         <div className='navbar'>
-            <div className='navbar-button'>
+            <div className='toggleButton'>
                 <button>
                 </button>
             </div>
-            <div className='navbar-links'>
+            <div className='links'>
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/resume'>Resume</Link>
