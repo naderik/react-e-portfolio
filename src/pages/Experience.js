2968bfa297fa7db5946d9f921c088b6fd5ffc8dd
Kasra Naderi <kasra@yuser.co>
2022-06-07 13:17:21 -0400

    revert experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -13,20 +13,39 @@ function Experience() {
     <div className="experience">
       <VerticalTimeline lineColor="#0F343E">
         <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2014 - 2018"
+          className="vertical-timeline-element--work"
+          date="2021 - present"
           contentStyle={{ background: "#0F343E", color: "#C5DA8D" }}
           iconStyle={{ background: "#0F343E", color: "#C5DA8D" }}
-          icon={<SchoolIcon />}
+          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Islamic Azad University
+            Full Stack Developer - Yuser Inc.
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            Bachelor's Degree
+            London, Ontario
           </h4>
-          <p>IT Engineering</p>
+          <p>
+            Helped the team launch 2 major features by working both in the front
+            end and back end.
+          </p>
         </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2020 - 2021"
+          contentStyle={{ background: "#0F343E", color: "#C5DA8D" }}
+          iconStyle={{ background: "#0F343E", color: "#C5DA8D" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Software Developer Intern - University of Windsor
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Windsor, Ontario
+          </h4>
+          <p>Developed the backend infrastructure for 3 projects.</p>
+        </VerticalTimelineElement>
+
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
           date="2020 - 2021"
@@ -45,38 +64,34 @@ function Experience() {
           <p> Applied Computing </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2020 - 2021"
+          className="vertical-timeline-element--education"
+          date="2014 - 2018"
           contentStyle={{ background: "#0F343E", color: "#C5DA8D" }}
           iconStyle={{ background: "#0F343E", color: "#C5DA8D" }}
-          icon={<WorkIcon />}
+          icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Software Developer - University of Windsor
+            Islamic Azad University
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            Windsor, Ontario
+            Bachelor's Degree
           </h4>
-          <p>Developed the backend infrastructure for 3 projects.</p>
+          <p>IT Engineering</p>
         </VerticalTimelineElement>
-
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2021 - present"
+          className="vertical-timeline-element--education"
+          date="2010 - 2014"
           contentStyle={{ background: "#0F343E", color: "#C5DA8D" }}
           iconStyle={{ background: "#0F343E", color: "#C5DA8D" }}
-          icon={<WorkIcon />}
+          icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Full Stack Developer - Yuser Inc.
+            High School Diploma
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            London, Ontario
+            Bachelor's Degree
           </h4>
-          <p>
-            Helped the team launch 2 major features by working both in the front
-            end and back end.
-          </p>
+          <p>Mathematics</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
