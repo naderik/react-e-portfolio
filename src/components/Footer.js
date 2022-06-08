97268259e42dac6fbd01ebbe4871b43f4c323e43
Kasra Naderi <kasra@yuser.co>
2022-06-08 15:21:56 -0400

    add social links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,12 +8,12 @@ function Footer() {
     return (
         <div className='footer'>
             <div className='socialMedia'>
-                <GitHubIcon />
-                <LinkedInIcon />
-                <TwitterIcon />
-            </div>
+                <a href='https://github.com/naderik' target="_blank" rel="noopener noreferrer"><GitHubIcon /></a>
+                <a href='https://linkedin.com/in/naderik' target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
+                <a href='https://twitter.com/naderik_kaz' target="_blank" rel="noopener noreferrer"><TwitterIcon /></a>
+            </div >
             <p> &copy; 2022 naderik.info </p>
-        </div>
+        </div >
     )
 }
 
