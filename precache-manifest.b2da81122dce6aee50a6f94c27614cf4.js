f8af6fd74723d51a69a433904c3a952cf7b1094d
Kasra Naderi <kasra@yuser.co>
2022-06-08 16:54:38 -0400

    Deploy E-Portfolio to GitHub Pages

---
+++ precache-manifest.b2da81122dce6aee50a6f94c27614cf4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e981698dece0459c0e1",
+    "url": "/e-portfolio/static/css/main.d180e565.chunk.css"
+  },
+  {
+    "revision": "8e981698dece0459c0e1",
+    "url": "/e-portfolio/static/js/main.8e981698.chunk.js"
+  },
+  {
+    "revision": "f643efff6e433eee2960",
+    "url": "/e-portfolio/static/js/1.f643efff.chunk.js"
+  },
+  {
+    "revision": "05be9ca625d45a0659e9",
+    "url": "/e-portfolio/static/css/2.c5e965ff.chunk.css"
+  },
+  {
+    "revision": "05be9ca625d45a0659e9",
+    "url": "/e-portfolio/static/js/2.05be9ca6.chunk.js"
+  },
+  {
+    "revision": "b9af77ab73bfb799ee3a",
+    "url": "/e-portfolio/static/js/runtime~main.b9af77ab.js"
+  },
+  {
+    "revision": "695b3833be63aa9b1e8fdd70bf5faf86",
+    "url": "/e-portfolio/static/media/Proj1.695b3833.png"
+  },
+  {
+    "revision": "ca9745459de645cbea7eb8a9f671cac2",
+    "url": "/e-portfolio/static/media/titillium-web-latin-ext-400-normal.ca974545.woff2"
+  },
+  {
+    "revision": "0ef99cf07a2a261ab43d5dc1937ffb27",
+    "url": "/e-portfolio/static/media/titillium-web-latin-400-normal.0ef99cf0.woff2"
+  },
+  {
+    "revision": "f6b4a4c0ae99563f0234681157481231",
+    "url": "/e-portfolio/static/media/titillium-web-all-400-normal.f6b4a4c0.woff"
+  },
+  {
+    "revision": "2b4a3c9332bf4575edc28facb59b6fda",
+    "url": "/e-portfolio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/e-portfolio/precache-manifest.b2da81122dce6aee50a6f94c27614cf4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/e-portfolio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
