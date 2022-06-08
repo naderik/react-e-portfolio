0c09cb87e339d72b780bb8865dd9f91715d355f7
Kasra Naderi <kasra@yuser.co>
2022-06-08 16:41:41 -0400

    remove unused code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,29 +17,6 @@ function Home() {
                     <a href='https://github.com/naderik' target="_blank" rel="noopener noreferrer"><GitHubIcon /></a>
                 </div>
             </div>
-            {/* <div className="skills">
-                <h1> Skills</h1>
-                <ol className="list">
-                    <li className="item">
-                        <h2> Front-End</h2>
-                        <span>
-                            ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-                            Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
-                        </span>
-                    </li>
-                    <li className="item">
-                        <h2>Back-End</h2>
-                        <span>
-                            NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-                            MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
-                        </span>
-                    </li>
-                    <li className="item">
-                        <h2>Languages</h2>
-                        <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
-                    </li>
-                </ol>
-            </div> */}
         </div>
     );
 }
