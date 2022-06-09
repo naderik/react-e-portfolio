bf8c6abd2e8f1e77f518e48ee896cad2ba7012b4
Kasra Naderi <kasra@yuser.co>
2022-06-09 09:04:59 -0400

    add second project to projects

--- src/data/ProjectList.js
+++ src/data/ProjectList.js
@@ -1,10 +1,17 @@
 import Proj1 from "../assets/Proj1.png";
+import Proj2 from "../assets/Proj2.png";
 
 export const ProjectList = [
   {
-    name: "E- Portfolio Website",
+    name: "React Personal E- Portfolio",
     image: Proj1,
-    skills: "React, HTML, CSS, MaterialUI",
-    github: "https://www.github.com/naderik/E-Portfolio",
+    skills: "Javasript, React, HTML, CSS, MaterialUI",
+    github: "https://www.github.com/naderik/react-e-portfolio",
   },
+  {
+    name: "Bootstrap Personal E- Portfolio",
+    image: Proj2,
+    skills: "Javascript, jQuery, HTML, CSS, Bootstrap",
+    github: "https://www.github.com/naderik/naderik.github.io",
+  }
 ];
