6aa2b2b8843daaa39637860a130d864e09821a2a
Kasra Naderi <kasra@yuser.co>
2022-06-10 23:55:31 -0400

    add testid to navbar

---
+++ src/components/Navbar.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "./Navbar";
+
+describe("Navbar", () => {
+    it("renders Navbar", () => {
+        render(<Navbar />);
+    }
+    );
+    expect(screen.getByText("Home")).toBeInTheDocument();
+}
+);
