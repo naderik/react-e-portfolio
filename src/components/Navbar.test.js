bcfe515d085de15c0d02e1ee08efe19983a8069e
Kasra Naderi <kasra@yuser.co>
2022-06-11 00:02:15 -0400

    small fix

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -4,8 +4,7 @@ import Navbar from "./Navbar";
 describe("Navbar", () => {
     it("renders Navbar", () => {
         render(<Navbar />);
-    }
-    );
+    });
     expect(screen.getByTestId("navbar")).toBeInTheDocument();
 }
 );
