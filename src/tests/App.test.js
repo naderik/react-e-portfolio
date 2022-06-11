3e0a2a3e0d7eedd3b9746f7077e223ea41bb80f7
Kasra Naderi <kasra@yuser.co>
2022-06-11 15:01:57 -0400

    make all the tests pass

---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import React from 'react';
+
+test('renders react component', async () => {
+  render(
+    <App />
+  );
+});
---
+++ src/tests/components/Navbar.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "../../components/Navbar";
+import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+
+describe("Navbar", () => {
+    it("renders Navbar", () => {
+        render(
+            <Router>
+                <Navbar />
+            </Router>);
+    });
+}
+);
