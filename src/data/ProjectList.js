f423a2a10f8076e7193d3c67833d4697b7fa541d
Kasra Naderi <kasra@yuser.co>
2022-06-23 15:15:47 -0400

    add crypto-51 to projects

--- src/data/ProjectList.js
+++ src/data/ProjectList.js
@@ -1,5 +1,6 @@
 import Proj1 from "../assets/Proj1.png";
 import Proj2 from "../assets/Proj2.png";
+import Proj3 from "../assets/Proj3.png";
 
 export const ProjectList = [
   {
@@ -13,5 +14,11 @@ export const ProjectList = [
     image: Proj2,
     skills: "Javascript, jQuery, HTML, CSS, Bootstrap",
     github: "https://www.github.com/naderik/naderik.github.io",
+  },
+  {
+    name: "Crypto-51",
+    image: Proj3,
+    skills: "TypeScript, NextJS, Blockchain, Web3, Moralis, TailwindCSS",
+    github: "https://www.github.com/naderik/crypto-51",
   }
 ];
